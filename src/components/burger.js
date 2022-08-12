957e2a1cce1a2e6ee85a6915e3b684f33443d512
kuldeep <kuldeep.singh.cs23@iilmcet.ac.in>
2022-08-12 11:36:34 +0530

    initialise project

---
+++ src/components/burger.js
@@ -0,0 +1,8 @@
+
+  burger = document.querySelector('.burger')
+  navbar = document.querySelector('.navbar')
+  rightnav= document.querySelector('.rightnav')
+  navList= document.querySelector('.navlist')
+  burger.addEventListener('click',()=>{
+    navbar.classList.toggle('v-class')
+  })
