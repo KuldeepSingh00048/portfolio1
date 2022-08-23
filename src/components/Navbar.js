b464a131d4201f5477291b82fb00ab955b876204
kuldeep <kuldeep.singh.cs23@iilmcet.ac.in>
2022-08-23 12:42:49 +0530

    up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
-import {NavLink} from 'react-router-dom';
+// import {NavLink} from 'react-router-dom'
 import logo from "../images/logo33.jpg";
 const Navbar = () => {
   const [current,update] = useState(false);
 
-  
   return (
     <>
       <nav  className= {current ?"navbar" : "nav-h"} id='navbar'>
                  <ul className={current ? "navlist" : "navlist-res"} id='navlist'>
                   <div class='logo'><img src={logo}/></div>
-                   <li><NavLink href='/'>Home</NavLink></li>
-                   <li><NavLink href='/about'>About_Me</NavLink></li>
-                   <li><NavLink href='/qualification'>Qualification</NavLink></li>
-                   <li><NavLink href='/project'>Projects</NavLink></li>
-                   <li><NavLink href='/intern'>Internship</NavLink></li>
-                   <li><NavLink href='/review'>Review</NavLink></li>
+                   <li><a href='/'>Home</a></li>
+                   <li><a href='/about'>About_Me</a></li>
+                   <li><a href='/qualification'>Qualification</a></li>
+                   <li><a href='/project'>Projects</a></li>
+                   <li><a href='/intern'>Internship</a></li>
+                   <li><a href='/review'>Review</a></li>
               </ul>
                   <div className={current ? "rightnav" : "rightnav-res"} id='rightnav'>
-                     <NavLink href='contact'><button>Contact</button></NavLink> 
+                     <a href='contact'><button>Contact</button></a> 
                  </div> 
                 
                  <button className='burger' onClick={()=> update(!current)}>
